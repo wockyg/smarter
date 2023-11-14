d6c4564f0dae133501aa61e3b33a35122636bc92
Warren <wmcclure1016@gmail.com>
2023-11-14 14:40:09 -0500

    Revert "bandaid"
    This reverts commit 33c5cfb845927f37c0d485fcfe98054f71695f2b.

--- src/map/MapTab.js
+++ src/map/MapTab.js
@@ -23,8 +23,7 @@ import selectedPinFCE from '../img/selectedPinFCE.png';
 
 import { useParams } from 'react-router-dom';
 
-// eslint-disable-next-line
-import Map, {NavigationControl, Marker, Popup} from '!react-map-gl';
+import Map, {NavigationControl, Marker, Popup} from 'react-map-gl';
 
 import {setDefaults, fromAddress, geocode, RequestType} from "react-geocode";
 
