cacc4358e2fff0bade2cd23a83fe2192943516b0
Warren <wmcclure1016@gmail.com>
2024-04-08 18:58:31 -0400

    fixed order uploads table for sure

--- src/billing-tab/D1500NotApproved.js
+++ src/billing-tab/D1500NotApproved.js
@@ -93,9 +93,9 @@ export default function D1500NotApproved(props) {
     const { status: statusBills, data: bills, error: errorBills, isFetching: isFetchingBills } = useGetV1500NotApproved();
     const { status: statusCptRows, data: cptRows, error: errorCptRows, isFetching: isFetchingCptRows } = useGetV1500RowsNotApproved();
 
-    // console.log(cptRows);
+    console.log(bills);
 
-    const rowsSorted = bills?.sort((a, b) => -b[initialSort]?.localeCompare(a[initialSort]));
+    const rowsSorted = bills?.sort((a, b) => -b[initialSort] > a[initialSort]);
 
     return (
         <>
