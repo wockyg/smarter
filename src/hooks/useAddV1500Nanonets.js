eebaf0437511d1f574fd91b0790229ea4e47d0db
Warren <wmcclure1016@gmail.com>
2024-04-23 14:34:41 -0400

    ok

--- src/hooks/useAddV1500Nanonets.js
+++ src/hooks/useAddV1500Nanonets.js
@@ -9,7 +9,7 @@ export default function useAddV1500Nanonets() {
 
   const queryClient = useQueryClient();
 
-  const addV1500 = async (values) => {
+  const addV1500 = (values) => {
 
     // console.log(values)
 
@@ -17,32 +17,28 @@ export default function useAddV1500Nanonets() {
 
     // console.log(files)
 
-    await Promise.all(
-      files.map((file, index) => {
+    return Promise.all(
+
+      files.map(async (file, index) => {
+
         const formData = new FormData();
         formData.append("v1500Blobs", file);
         formData.append("userId", values.get("userId"))
-        return new Promise((resolve, reject) => {
-          api.post(
-          '/v1500/upload/smarter/nanonets', 
-          formData, 
-          {
-            headers: {'Content-Type': 'multipart/form-data'},
-            onUploadProgress: (p) => {
-              const percentComplete = Math.round((p.loaded * 100) / p.total)
-              setV1500UploadProgress({filename: file.name, percentComplete: percentComplete})
-              console.log(`${percentComplete}% uploaded`)
+
+        return await api.post(
+            '/v1500/upload/smarter/nanonets', 
+            formData, 
+            {
+              headers: {'Content-Type': 'multipart/form-data'},
+              onUploadProgress: (p) => {
+                  const percentComplete = Math.round((p.loaded * 100) / p.total)
+                  setV1500UploadProgress({filename: file.name, percentComplete: percentComplete})
+                  console.log(`${percentComplete}% uploaded`)
+                }
             }
-          }).then(data => {
-            console.log(`file ${index+1} uploaded?`)
-            console.log(data)
-            resolve(data)
-          })
-        })
+          )
       })
-    ).then(res => {
-      return res
-    })
+    )
       
   }
     
