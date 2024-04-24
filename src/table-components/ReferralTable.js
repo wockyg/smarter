164ab452ac1a2af476603860d101e9398fa77699
Warren <wmcclure1016@gmail.com>
2024-04-24 14:21:40 -0400

    ok maybe I understand

--- src/table-components/ReferralTable.js
+++ src/table-components/ReferralTable.js
@@ -805,6 +805,12 @@ export default function ReferralTable(props) {
 
         formData.append("userId", user.userId)
 
+        const newProgress = uploadedFiles.map(f => {
+            return {filename: f.name, percentComplete: 0}
+        })
+
+        setV1500UploadProgress(newProgress)
+
         // method === 'sensible' && uploadedFiles.length > 0 && v1500AddSensible.mutate(formData)
 
         method === 'nanonets' && uploadedFiles.length > 0 && v1500AddNanonets.mutate(formData, {onSuccess:  (newData) => {setUploadComplete(true)}})
